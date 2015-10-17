c0a5db8afba9af26f723f0ad2102a2a513c0f94d
ares <ares@anghenfil.de>
2015-10-17 08:41:42 -0400

    leaflet libary, config file begin, formular begin

---
+++ formular.php
@@ -0,0 +1,7 @@
+<form>
+	<label for="Art of border">Art of border (Fance, branwire, etc.)</label>
+	<input id="Art of border" minlength="1" maxlength="100"><br>
+	
+	<label for="check1">Duration</label>
+	<input id="check1" 
+</form>
---
+++ includer.php
@@ -0,0 +1,4 @@
+<?php
+require_once("includes/config.php"); //Load config file
+require_once("includes/lib/autoloader.php"); //Autoloader for Classes
+
