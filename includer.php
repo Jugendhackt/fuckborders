45b44673c856313a5a1e677df2c4b43b67f43ee1
ares <ares@anghenfil.de>
2015-10-23 10:05:13 -0400

    loader inclusion

--- includer.php
+++ includer.php
@@ -2,5 +2,5 @@
 require_once("includes/config.php"); //Load config file
 require_once("includes/lib/autoloader.php"); //Autoloader for Classes
 require_once("includes/header.php"); //The Header
-require_once("includes/map.php"); //w
-require_once("includes/footer.php");
+require_once("includes/map.php"); //Include the Map
+require_once("includes/footer.php"); //The Footer ...
