b121560c1fa815a5cc26194a24f5274e90e2d0a8
ares <ares@anghenfil.de>
2015-10-23 10:03:27 -0400

    Includes Bootstrap (css and javascript)

--- includes/header.php
+++ includes/header.php
@@ -6,8 +6,10 @@
 	<title>Fuck Borders</title>
 	<link rel="stylesheet" href="includes/resources/style.css" />
 	<link rel="stylesheet" href="includes/resources/leaflet/leaflet.css" />
+	<link rel="stylesheet" href="includes/resources/bootstrap/css/bootstrap.min.css" />
 	<link rel="stylesheet" href="includes/resources/form.css" />
 	<link href='https://fonts.googleapis.com/css?family=Cinzel' rel='stylesheet' type='text/css'>
+	<script src="includes/resources/bootstrap/js/bootstrap.min.js"></script>
 </head>
 
 <body>
